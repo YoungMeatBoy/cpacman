e433b1765e0c2e023e490ae4445e2e694df82954
YoungMeatBoy <d2ms2nk@gmail.com>
2020-04-19 13:48:06 +0300

    prepush commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 		name             = 'cpacman',
-		version          = '1.0.1',
+		version          = '1.0.2',
 		packages         = find_packages(),
 		author           = 'Miasnenko Dmitry',
 		author_email     = 'cl0wzed.exe@gmail.com',
